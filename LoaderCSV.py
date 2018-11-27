653c304315288aba5e18a31f4389fb1045ae162c
JoseManuel <jmba12@gcloud.ua.es>
2018-11-27 15:59:25 +0100

    Added first line for branching

--- LoaderCSV.py
+++ LoaderCSV.py
@@ -1,5 +1,6 @@
 import pandas as pd;
 
+# We must specify ";" delimiter with our dataset (the default delimitr is "," and it gives us problems)
 rawturbdata = pd.read_csv("C:/Users/Buba/OneDrive/LucentiaLab/DemoClustering141218/CopiaDatosMATLAB.csv", delimiter=";");
 
 print(rawturbdata);
